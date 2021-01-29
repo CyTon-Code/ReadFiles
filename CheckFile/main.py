254470d9197abe5b6b959b6dc434a889f373bbf7
CyTon Program Languages <cytoncode@gmail.com>
2021-01-29 20:49:35 +0200

    The file does not exist?
    Module (Library) to check if a file exists.
    If the file does not exist, then True, otherwise - False or None.

--- CheckFile/main.py
+++ CheckFile/main.py
@@ -27,6 +27,7 @@ def CheckFile(AddressFile):  # Check if the file exists:
   except:  # The file did not open:
     print("File is not defined. File:", AddressFile)
     FileOpen = False  # At the end of the function, the file cannot be closed (it has not been opened).
+    
     return not FileOpen  # Answer: The file is missing or has not been read.
   
   finally:
