687f1e1cb6e511754010ef81e547a0adee7c7917
CyTon Program Languages <cytoncode@gmail.com>
2021-01-29 19:03:52 +0200

    Does the file exist?
    Module (Library) to check if a file exists.
    If the file exists, then True, otherwise - False.

--- CheckFile/main.py
+++ CheckFile/main.py
@@ -1,10 +1,10 @@
 """The module works only through import.
-    Via os.system or return (RUN) - doesn't work."""
+  Via os.system or return (RUN) - doesn't work."""
 
 
 if __name__ == "__main__":  # If not imported, I exit is the module:
-print("I am is Module!!! Bye Bye!!!")    
-exit()  # Answer: I'm leaving, I'm a module.
+  print("I am is Module!!! Bye Bye!!!")    
+  exit()  # Answer: I'm leaving, I'm a module.
 
 
 def CheckFile(AddressFile):  # Check if the file exists:
