ba6b3e5b1b41caf9e5d82643d6354616a6b4b6c1
CyTon Program Languages <cytoncode@gmail.com>
2021-01-29 19:28:13 +0200

    Does the file exist?
    Module (Library) to check if a file exists.
    If the file exists, then True, otherwise - False.

--- CheckFile/main.py
+++ CheckFile/main.py
@@ -5,34 +5,35 @@
 if __name__ == "__main__":  # If not imported, I exit is the module:
   print("I am is Module!!! Bye Bye!!!")    
   exit()  # Answer: I'm leaving, I'm a module.
-
+  pass
 
 def CheckFile(AddressFile):  # Check if the file exists:
   """This module is required to read a file (or module).
   It can also check if a file (or module) exists."""
   # Credo: Checking file readability...
-  Process = False
+  
+  FileOpen = True  
 
   try:  # Opens a file:
     File = open(AddressFile, 'r')
 
     try:  # The file is read:
       File.read()
-      Process = True  # Answer: The file is there, and it has been read.
+      return True  # Answer: The file is there, and it has been read.
         
     except:  # File not read:
-      Process = None  # Answer: The file is present, but not readable.
+      return None  # Answer: The file is present, but not readable.
     
   except:  # The file did not open:
     print("File is not defined. File:", AddressFile)
-    Process = False  # Answer: The file is missing or has not been read.
-    
+    FileOpen = False  # At the end of the function, the file cannot be closed (it has not been opened).
+    return FileOpen  # Answer: The file is missing or has not been read.
+  
   finally:
     try:  # Closes the file:
       File.close()
 
     except:  # The file didn't close:
-      if (Process != False):
-        raise Exception  # Answer: The file won't close! We throw in the program: "Exception".
-    
-  return Process
+      if not FileOpen:  # If the file is not open:
+        raise TheFileDidntClose  # Answer: The file won't close! We throw in the program: "Exception".
+  pass
