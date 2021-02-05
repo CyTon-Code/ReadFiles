0ac96e21f3ddc3e56478df1858d901f2f7cc36e5
CyTon Code <cytoncode@gmail.com>
2021-02-05 23:43:19 +0200

    Update lib.py and all

---
+++ Turd/1.py
@@ -0,0 +1,40 @@
+def whats_file_name(path_file: str):  # "read/terminals/index.terminal"
+    # Если это не ссылка а мусор то сваливаю:
+    if path_file == "" or path_file == " " or path_file == "\n" or path_file == "\t":
+        return None
+
+    # Если это не ссилка, то это имя файла:
+    if "/" not in path_file and "\\" not in path_file:
+        return path_file
+
+    # Возвращаю имя файла:
+
+    # name = ''
+    i = 0
+    n = len(path_file)-1
+    while i <= n:
+        if path_file[n] in "\\/":
+            break
+        name = path_file[n] + name
+        n -= 1
+    print(name)
+    name = ''
+    for i in path_file:
+        name += i
+        if i in "\\/":
+            name = ''
+
+    return whats_file_name(name)
+
+
+tmp = whats_file_name("read/terminals/index.terminal")
+print(tmp, type(tmp))
+
+tmp = whats_file_name("read/terminals/index.terminal/")
+print(tmp, type(tmp))
+
+tmp = whats_file_name("index.terminal")
+print(tmp, type(tmp))
+
+tmp = whats_file_name("")
+print(tmp, type(tmp))
