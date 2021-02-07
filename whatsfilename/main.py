15e2628664734729a4c4a801c0d862bc71f697ea
CyTon Code <cytoncode@gmail.com>
2021-02-07 12:53:45 +0200

    Update whatsfilename

--- whatsfilename/main.py
+++ whatsfilename/main.py
@@ -1,14 +1,31 @@
-def equal(a: str, b: str):
-    return a == b
+"""
+    The module works only through import.
+    Via os.system or return (RUN) - doesn't work.
+"""
+
+if __name__ == "__main__":  # If not imported, I exit is the module:
+    print("I am is Module!!! Bye Bye!!!")
+    exit()  # Answer: I'm leaving, I'm a module.
+
+
+def cut_name(link):  # Cut filename from link:
+    name = ''
+    for i in link:
+        name += i
+        if i in "\\/":
+            name = ''
+    return name
 
 
 def whats_file_name(link: str):
-    """Expected example:\n
-    whats_file_name("read/terminals/index.terminal")"""
+    """
+        I am processing file addresses and returning the filename.
+        Using delimiters: / \\
+    """
 
     # If this is not a link, but garbage, then exit:
-    if equal(link, "") or equal(link, " ") or (
-            equal(link, "\n") or equal(link, "\t")):
+    if link == "" or link == " " or (
+            link == "\n" or link == "\t"):
         return None
 
     # If it is not a link, then this is the filename:
@@ -16,11 +33,7 @@ def whats_file_name(link: str):
         return link
 
     # Cut filename from link:
-    name = ''
-    for i in link:
-        name += i
-        if i in "\\/":
-            name = ''
+    name = cut_name(link)
 
     # Return filename:
-    return whats_file_name(name)  # reprocessing
+    return whats_file_name(name)  # reprocessing...
