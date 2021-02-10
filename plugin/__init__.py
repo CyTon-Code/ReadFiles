74eba714d20a2c7324e136e5650f3afdf470fb99
CyTon Program Languages <cytoncode@gmail.com>
2021-02-10 17:54:46 +0200

    Update __init__.py

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -2,6 +2,7 @@ from typing import IO
 
 
 class FileDidNotClose(Exception):
+    # The file didn't close
     def __init__(self):
         pass
 
@@ -20,11 +21,11 @@ def file_read(file: IO) -> bool or None:
         return None  # Answer: The file is present, but not readable.
 
 
-def file_close(file: IO, error_code: str) -> None or Exception:
+def file_close(file: IO, commit: str) -> None or Exception:
     try:  # Closes the file:
         file.close()
 
     except:  # The file didn't close:
-        print(f"error: {error_code}")
+        print(f"error: {commit}")
         raise FileDidNotClose  # Answer: The file won't close! We
         # throw in the program: "Exception".
