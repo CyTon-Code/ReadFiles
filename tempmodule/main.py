c3144d392fd9b0dde6b585b9a139538add7b88bc
CyTon Program Languages <cytoncode@gmail.com>
2021-02-10 18:10:46 +0200

    Update main.py

--- tempmodule/main.py
+++ tempmodule/main.py
@@ -1,2 +1,23 @@
-def main(args: list) -> int:
+"""
+    The module works only through import.
+    Via os.system or return (RUN) - doesn't work.
+"""
+
+if __name__ == "__main__":  # If not imported, I exit is the module:
+    print("I am is Module!!! Bye Bye!!!")
+    exit()  # Answer: I'm leaving, I'm a module.
+
+
+def main(args: list) -> int:  # I work with files somehow:
+    # Make a layout before working with files.
+
+    # You must know the exact purpose before building a house.
+
+    # Feel free to use ready-made functions.
+
+    # The function should return working (processed and understandable)
+    #resources,
+
+    # If you have the strength to hold the sky, then you do it, not others.
+
     return 0
