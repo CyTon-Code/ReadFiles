36613274f5921ad02130882382bd7b7eade55b14
CyTon Program Languages <cytoncode@gmail.com>
2021-02-10 18:14:19 +0200

    Update main.py

--- edit/main.py
+++ edit/main.py
@@ -9,29 +9,34 @@ from plugin import file_close
 if __name__ == "__main__":  # If not imported, I exit is the module:
     print("I am is Module!!! Bye Bye!!!")
     exit()  # Answer: I'm leaving, I'm a module.
-    pass
 
 
-def edit_file(link: str) -> bool or Exception:  # Edit file if it exists:
-    """This module is required to read a file (or module).
-    It can also check if a file (or module) exists."""
-    # Credo: Checking file readability...
+def edit_file(link: str, line_of_text="") -> bool or Exception:  # Edit file if it exists:
+    """
+        This module is required to read a file (or module).
+        It can also check if a file (or module) exists.
+    """  # Credo: Checking file readability...
+
     if check_file(link):  # I only edit if the file exists:
-        pass
+        return None
 
     file_open = True
     file = None
 
-    try:  # create a file:
+    try:  # modify file:
         file = open(link, 'w')
-        return False  # Answer: The file is there.
-
-    except:  # The file did not create but it is live:
-        print("file is not created. file:", link)
+        
+        # editing a file:
+        file.write(line_of_text)
+        return True  # Answer: The file has been edited.
+    
+    except:  # The file has not been modified, but it exists:
+        # Failed to modify file:
+        print("the file has not changed. file:", link)
         file_open = False  # At the end of the function, the file cannot be
         # closed (it has not been opened).
-
-        return not file_open  # Answer: The file is missing or has not been read.
+        # Answer: The file was not edited.
+        return False  # Answer: The file is missing or has not been read.
 
     finally:
         if file_open:  # If the file open:
